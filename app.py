6ae9e14f3374b390787ff671c54b1741fe84341f
wederson <emrferramentas@gmail.com>
2026-06-08 17:02:21 -0300

    novo

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ def index():
 def simular():
     valor_inicial = float(request.form.get('investimento_inicial'))
     taxa_juros = float(request.form.get('taxa_juros_mensal'))
-    contribuicao_mensal = float(request.form.get('contribuicao_mensal'))
+    
     duracao_investimento = int(request.form.get('duracao_investimento'))
 
     valor_final = valor_inicial
     for _ in range(duracao_investimento * 1):
-        valor_final += contribuicao_mensal
+        
         valor_final += valor_final * (taxa_juros / 100) / 1
 
-    valor_total_investido = valor_inicial + (contribuicao_mensal * duracao_investimento * 12)
+    valor_total_investido = valor_inicial + (taxa_juros / 100) * duracao_investimento * 1
     ganho_total = valor_final - valor_total_investido
 
-    return render_template('index.html', valor_final=valor_final, valor_total_investido=valor_total_investido, ganho_total=ganho_total)
+    return render_template('index.html', valor_final=f"{valor_final:.2f}", valor_total_investido=f"{valor_total_investido:.2f}", ganho_total=f"{ganho_total:.2f}")
 if __name__ == '__main__':
     app.run(debug=True , port=5000 , host='0.0.0.0')
